d9d0031b8e48f0ebbf02093a3b4bd85fa8afa122
Francrack22 <fortega46910@ucq.edu.mx>
2026-07-22 08:06:28 -0600

    Update Home widget layout with Header and SearchBar
    Refactor Home widget to include Header and SearchBar.

--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
-import 'header.dart';
+import 'widgets/header.dart';
+import 'widgets/search_bar.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Header();
-  }  
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Header(),
+        const SizedBox(height: 20),
+        SearchBarWidget(
+          onCitySelected: (lat, lon, name) {
+            debugPrint("Ciudad seleccionada: $name -> Lat: $lat, Lon: $lon");
+          },
+        ),
+      ],
+    );
+  }
 }
