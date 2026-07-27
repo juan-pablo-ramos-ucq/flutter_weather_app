12951a95400e70c9b40dcec5caeb79097166fe8a
Juan Pablo Ramos Escalona <juan.ramos.dev@gmail.com>
2026-07-27 10:32:04 -0600

    Uncomment SearchBarWidget in Home widget for city selection functionality

--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'header.dart';
 import 'vacio.dart';
-//import 'search_bar.dart';
+import 'search_bar.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -18,11 +18,7 @@ class Home extends StatelessWidget {
               const Header(),
               const Vacio(),
               const SizedBox(height: 20),
-              // SearchBarWidget(
-              //   onCitySelected: (lat, lon, name) {
-              //     debugPrint("Ciudad seleccionada: $name -> Lat: $lat, Lon: $lon");
-              //   },
-              // ),
+              SearchBarWidget(),
             ],
           ),
         ),
